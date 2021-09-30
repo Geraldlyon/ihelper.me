21279113659939d37f248e2e33496c45e2c261ee
tomatograce <geraldlyonga@gmail.com>
2021-09-30 10:02:57 +0100

    first push of code to repo in development brach

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        color: "blue",
+        textAlign: "center",
+        padding: "1rem 0rem 1rem 0rem",
+      }}
+    >
+      <h3>
+        welcome <b>ihelper.com</b> @copy
+      </h3>
+    </div>
+  );
+}
+
+export default Footer;
